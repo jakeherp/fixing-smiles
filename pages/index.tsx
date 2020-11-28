0d3459cef92a0d8a3e7651c264bb95c1af4cc658
Jacob Herper <jacob@herper.uk>
2020-11-28 23:44:13 +0000

    added svg import support

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,9 @@
+import Logo from 'public/logo.svg';
+
 export default function Home() {
 	return (
 		<div className="md:flex bg-white rounded-lg p-24 justify-center">
+			<Logo />
 			<img
 				className="h-16 w-16 md:h-24 md:w-24 rounded-full mx-auto md:mx-0 md:mr-6"
 				src="/dulce.jpg"
